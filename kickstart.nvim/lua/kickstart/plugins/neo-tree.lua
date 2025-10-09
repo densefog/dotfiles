6d78fce64f4c3f7bc2b2988cd1541a854604d994
Troy Spruit <troy@threefivesix.com>
2025-10-09 10:35:16 -0400

    Switched tree

--- kickstart.nvim/lua/kickstart/plugins/neo-tree.lua
+++ kickstart.nvim/lua/kickstart/plugins/neo-tree.lua
@@ -1,27 +1,47 @@
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
+-- nvim-tree is a file explorer for Neovim
+-- https://github.com/nvim-tree/nvim-tree.lua
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
+  'nvim-tree/nvim-tree.lua',
   version = '*',
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
+    'nvim-tree/nvim-web-devicons',
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-    { '<Leader>n', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '\\', ':NvimTreeToggle<CR>', desc = 'NvimTree toggle', silent = true },
+    { '<Leader>n', ':NvimTreeFindFile<CR>', desc = 'NvimTree find file', silent = true },
   },
   opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-          ['<bs>'] = 'close_node',
+    disable_netrw = true,
+    hijack_netrw = true,
+    sync_root_with_cwd = true,
+    view = {
+      width = 30,
+    },
+    renderer = {
+      group_empty = true,
+      icons = {
+        show = {
+          git = true,
+          folder = true,
+          file = true,
+          folder_arrow = true,
         },
       },
+    },
+    filters = {
+      dotfiles = false,
+      custom = { '.git', '.DS_Store' },
+      git_ignored = true,
+      no_buffer = false,
+      exclude = {},
+    },
+    git = {
+      enable = true,
+      ignore = true,
+      show_on_dirs = true,
+      show_on_open_dirs = true,
     },
   },
 }
